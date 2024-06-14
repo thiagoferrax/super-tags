eb6e4718b494135f71b9bb02445c4ce7831f8a49
Micael Costa <micael.costa@celfinet.com>
2024-06-14 12:37:01 +0100

    add default configs and create a folder to create unit tests

--- project/jest.config.js
+++ project/jest.config.js
@@ -1,9 +1,9 @@
 const nextJest = require("next/jest");
 const createJestConfig = nextJest({
-  dir: "./",
+    dir: "./",
 });
 const customJestConfig = {
-  moduleDirectories: ["node_modules", "<rootDir>/"],
-  testEnvironment: "jest-environment-jsdom",
+    moduleDirectories: ["node_modules", "<rootDir>/"],
+    testEnvironment: "jest-environment-jsdom",
 };
 module.exports = createJestConfig(customJestConfig);
