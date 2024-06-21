fe2b2e9a8ed4d213b04740fd1d8a7d2a1e5b2eb2
Micael Costa <micael.costa@celfinet.com>
2024-06-21 20:06:43 +0100

    create user usecase - Should call userRepository with correct params and only once

--- project/src/backend/core/use-cases/create-user.test.ts
+++ project/src/backend/core/use-cases/create-user.test.ts
@@ -1,44 +1,88 @@
 import { faker } from "@faker-js/faker";
-import { CreateUser } from "./create-user";
+import { CreateUser, CreateUserParams } from "./create-user";
 import { ValidationError } from "../errors/validationError";
+import { IAddUserRepository } from "@/backend/db/user-repository";
+import { User } from "../models/user";
+
+class UserRepositoryMock implements IAddUserRepository {
+	user: User | undefined
+	count: number = 0
+	async Add(user: User): Promise<void> {
+		this.user = user
+		this.count++
+	}
+
+}
+
+type makeSutParams = {
+	name?: string,
+	email?: string,
+	password?: string
+
+}
+
+type makeSutResult = {
+	sut: CreateUser,
+	userRepository: UserRepositoryMock
+}
+
+
+const makeSut = (): makeSutResult => {
+	const userRepository = new UserRepositoryMock()
+	const sut = new CreateUser(userRepository);
+
+	return { sut, userRepository }
+}
 
 describe("CreateUser usecase tests", () => {
-    test('Should throw ValidationError if no name is provided', async () => {
-        const sut = new CreateUser();
-        await expect(() => sut.Execute({
-            name: undefined as any,
-            email: faker.internet.email(),
-            password: faker.internet.password()
-        })).rejects.toThrow(new ValidationError("CreateUserParams", "name", "NAME_REQUIRED"))
-    });
+	test('Should throw ValidationError if no name is provided', async () => {
+		const { sut } = makeSut()
+		await expect(() => sut.Execute({
+			name: undefined as any,
+			email: faker.internet.email(),
+			password: faker.internet.password()
+		})).rejects.toThrow(new ValidationError("CreateUserParams", "name", "NAME_REQUIRED"))
+	});
 
-    test('Should throw ValidationError if no email is provided', async () => {
-        const sut = new CreateUser();
-        await expect(() => sut.Execute({
-            name: faker.person.fullName(),
-            email: undefined as any,
-            password: faker.internet.password()
-        })).rejects.toThrow(new ValidationError("CreateUserParams", "email", "EMAIL_REQUIRED")
-        )
-    });
+	test('Should throw ValidationError if no email is provided', async () => {
+		const { sut } = makeSut()
+		await expect(() => sut.Execute({
+			name: faker.person.fullName(),
+			email: undefined as any,
+			password: faker.internet.password()
+		})).rejects.toThrow(new ValidationError("CreateUserParams", "email", "EMAIL_REQUIRED")
+		)
+	});
 
-    test('Should throw ValidationError if invalid email is provided', async () => {
-        const sut = new CreateUser();
-        await expect(() => sut.Execute({
-            name: faker.person.fullName(),
-            email: undefined as any,
-            password: faker.internet.password()
-        })).rejects.toThrow(new ValidationError("CreateUserParams", "email", "INVALID_EMAIL")
-        )
-    });
+	test('Should throw ValidationError if invalid email is provided', async () => {
+		const { sut } = makeSut()
+		await expect(() => sut.Execute({
+			name: faker.person.fullName(),
+			email: undefined as any,
+			password: faker.internet.password()
+		})).rejects.toThrow(new ValidationError("CreateUserParams", "email", "INVALID_EMAIL")
+		)
+	});
 
 	test('Should throw ValidationError if no password is provided', async () => {
-        const sut = new CreateUser();
-        await expect(() => sut.Execute({
-            name: faker.person.fullName(),
-            email: faker.internet.email(),
-            password: undefined as any,
-        })).rejects.toThrow(new ValidationError("CreateUserParams", "password", "PASSWORD_REQUIRED")
-        )
-    });
+		const { sut } = makeSut()
+		await expect(() => sut.Execute({
+			name: faker.person.fullName(),
+			email: faker.internet.email(),
+			password: undefined as any,
+		})).rejects.toThrow(new ValidationError("CreateUserParams", "password", "PASSWORD_REQUIRED")
+		)
+	});
+
+	test('Should call userRepository with correct params and only once', async () => {
+		const { sut, userRepository } = makeSut()
+		const sutParams = {
+			name: faker.person.fullName(),
+			email: faker.internet.email(),
+			password: faker.internet.password(),
+		}
+		await sut.Execute(sutParams)
+		expect(userRepository.user).toEqual(new User(null, sutParams.name, sutParams.email, sutParams.password))
+		expect(userRepository.count).toBe(1)
+	});
 })
--- project/src/backend/core/use-cases/create-user.ts
+++ project/src/backend/core/use-cases/create-user.ts
@@ -1,30 +1,34 @@
+import { IAddUserRepository } from "@/backend/db/user-repository"
 import { ValidationError } from "../errors/validationError"
+import { User } from "../models/user"
 
 export type CreateUserParams = {
-    name: string,
-    email: string,
-    password: string
+	name: string,
+	email: string,
+	password: string
 }
 
 const REGEX_EMAIL = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
 export class CreateUser {
-    constructor() {
+	constructor(private readonly addUserRepository: IAddUserRepository) {
 
-    }
-    async Execute(params: CreateUserParams): Promise<void> {
-        if (!params.name) {
-            throw new ValidationError("CreateUserParams", "name", "NAME_REQUIRED")
-        }
-        if (!params.email) {
-            throw new ValidationError("CreateUserParams", "email", "EMAIL_REQUIRED")
-        }
+	}
+	async Execute(params: CreateUserParams): Promise<void> {
+		if (!params.name) {
+			throw new ValidationError("CreateUserParams", "name", "NAME_REQUIRED")
+		}
+		if (!params.email) {
+			throw new ValidationError("CreateUserParams", "email", "EMAIL_REQUIRED")
+		}
 
-        if (!REGEX_EMAIL.test(params.email)) {
-            throw new ValidationError("CreateUserParams", "email", "INVALID_EMAIL")
-        }
+		if (!REGEX_EMAIL.test(params.email)) {
+			throw new ValidationError("CreateUserParams", "email", "INVALID_EMAIL")
+		}
 
 		if (!params.password) {
-            throw new ValidationError("CreateUserParams", "password", "PASSWORD_REQUIRED")
-        }
-    }
+			throw new ValidationError("CreateUserParams", "password", "PASSWORD_REQUIRED")
+		}
+
+		await this.addUserRepository.Add(new User(null, params.name, params.email, params.password))
+	}
 }
