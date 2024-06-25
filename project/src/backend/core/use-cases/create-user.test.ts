03167c2960be392f61740af546f19a8f6bb57bed
Micael Costa <micael.costa@celfinet.com>
2024-06-25 19:19:43 +0100

    add validation to have a strong password

--- project/src/backend/core/use-cases/create-user.test.ts
+++ project/src/backend/core/use-cases/create-user.test.ts
@@ -5,84 +5,98 @@ import { IAddUserRepository } from "@/backend/db/user-repository";
 import { User } from "../models/user";
 
 class UserRepositoryMock implements IAddUserRepository {
-	user: User | undefined
-	count: number = 0
-	async Add(user: User): Promise<void> {
-		this.user = user
-		this.count++
-	}
-
-}
-
-type makeSutParams = {
-	name?: string,
-	email?: string,
-	password?: string
+    user: User | undefined
+    count: number = 0
+    async Add(user: User): Promise<void> {
+        this.user = user
+        this.count++
+    }
 
 }
 
 type makeSutResult = {
-	sut: CreateUser,
-	userRepository: UserRepositoryMock
+    sut: CreateUser,
+    userRepository: UserRepositoryMock
 }
 
 
 const makeSut = (): makeSutResult => {
-	const userRepository = new UserRepositoryMock()
-	const sut = new CreateUser(userRepository);
+    const userRepository = new UserRepositoryMock()
+    const sut = new CreateUser(userRepository);
 
-	return { sut, userRepository }
+    return { sut, userRepository }
 }
 
 describe("CreateUser usecase tests", () => {
-	test('Should throw ValidationError if no name is provided', async () => {
-		const { sut } = makeSut()
-		await expect(() => sut.Execute({
-			name: undefined as any,
-			email: faker.internet.email(),
-			password: faker.internet.password()
-		})).rejects.toThrow(new ValidationError("CreateUserParams", "name", "NAME_REQUIRED"))
-	});
+    test('Should throw ValidationError if no name is provided', async () => {
+        const { sut } = makeSut()
+        await expect(() => sut.Execute({
+            name: undefined as any,
+            email: faker.internet.email(),
+            password: faker.internet.password()
+        })).rejects.toThrow(new ValidationError("CreateUserParams", "name", "NAME_REQUIRED"))
+    });
 
-	test('Should throw ValidationError if no email is provided', async () => {
-		const { sut } = makeSut()
-		await expect(() => sut.Execute({
-			name: faker.person.fullName(),
-			email: undefined as any,
-			password: faker.internet.password()
-		})).rejects.toThrow(new ValidationError("CreateUserParams", "email", "EMAIL_REQUIRED")
-		)
-	});
+    test('Should throw ValidationError if no email is provided', async () => {
+        const { sut } = makeSut()
+        await expect(() => sut.Execute({
+            name: faker.person.fullName(),
+            email: undefined as any,
+            password: faker.internet.password()
+        })).rejects.toThrow(new ValidationError("CreateUserParams", "email", "EMAIL_REQUIRED")
+        )
+    });
 
-	test('Should throw ValidationError if invalid email is provided', async () => {
-		const { sut } = makeSut()
-		await expect(() => sut.Execute({
-			name: faker.person.fullName(),
-			email: undefined as any,
-			password: faker.internet.password()
-		})).rejects.toThrow(new ValidationError("CreateUserParams", "email", "INVALID_EMAIL")
-		)
-	});
+    test('Should throw ValidationError if invalid email is provided', async () => {
+        const { sut } = makeSut()
+        await expect(() => sut.Execute({
+            name: faker.person.fullName(),
+            email: undefined as any,
+            password: faker.internet.password()
+        })).rejects.toThrow(new ValidationError("CreateUserParams", "email", "INVALID_EMAIL")
+        )
+    });
 
-	test('Should throw ValidationError if no password is provided', async () => {
-		const { sut } = makeSut()
-		await expect(() => sut.Execute({
-			name: faker.person.fullName(),
-			email: faker.internet.email(),
-			password: undefined as any,
-		})).rejects.toThrow(new ValidationError("CreateUserParams", "password", "PASSWORD_REQUIRED")
-		)
-	});
+    test('Should throw ValidationError if no password is provided', async () => {
+        const { sut } = makeSut()
+        await expect(() => sut.Execute({
+            name: faker.person.fullName(),
+            email: faker.internet.email(),
+            password: undefined as any,
+        })).rejects.toThrow(new ValidationError("CreateUserParams", "password", "PASSWORD_REQUIRED")
+        )
+    });
 
-	test('Should call userRepository with correct params and only once', async () => {
-		const { sut, userRepository } = makeSut()
-		const sutParams = {
-			name: faker.person.fullName(),
-			email: faker.internet.email(),
-			password: faker.internet.password(),
-		}
-		await sut.Execute(sutParams)
-		expect(userRepository.user).toEqual(new User(null, sutParams.name, sutParams.email, sutParams.password))
-		expect(userRepository.count).toBe(1)
-	});
+    test.each([
+        faker.word.noun(faker.number.int({ min: 1, max: 7 })),
+        faker.internet.password({ length: 9, pattern: /[a-z0-9!@#\$%\^&\*]/ }),
+        faker.internet.password({ length: 9, pattern: /[A-Z0-9!@#\$%\^&\*]/ }),
+        faker.internet.password({ length: 9, pattern: /[A-Za-z!@#\$%\^&\*]/ }),
+        faker.internet.password({ length: 9, pattern: /[A-Za-z0-9]/ }),
+    ])('Should throw ValidationError if provided password not have 8 caracters', async (password) => {
+        const { sut } = makeSut()
+        await expect(() => sut.Execute({
+            name: faker.person.fullName(),
+            email: faker.internet.email(),
+            password,
+        })).rejects.toThrow(new ValidationError("CreateUserParams", "password", "PASSWORD_NOT_STRONG_ENOUGH")
+        )
+    });
+
+
+    test('Should call userRepository with correct params and only once', async () => {
+        const { sut, userRepository } = makeSut()
+        const password = faker.internet.password({ length: 2, pattern: /[A-Z]/ }) +
+            faker.internet.password({ length: 2, pattern: /[a-z]/ }) +
+            faker.internet.password({ length: 2, pattern: /[0-9]/ }) +
+            faker.internet.password({ length: 2, pattern: /[!@#\$%\^&\*]/ })
+        const sutParams = {
+            name: faker.person.fullName(),
+            email: faker.internet.email(),
+            password,
+        }
+        await sut.Execute(sutParams)
+        expect(userRepository.user).toEqual(new User(null, sutParams.name, sutParams.email, sutParams.password))
+        expect(userRepository.count).toBe(1)
+    });
 })
--- project/src/backend/core/use-cases/create-user.ts
+++ project/src/backend/core/use-cases/create-user.ts
@@ -3,32 +3,37 @@ import { ValidationError } from "../errors/validationError"
 import { User } from "../models/user"
 
 export type CreateUserParams = {
-	name: string,
-	email: string,
-	password: string
+    name: string,
+    email: string,
+    password: string
 }
 
 const REGEX_EMAIL = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+const REGEX_PASSWORD = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/
 export class CreateUser {
-	constructor(private readonly addUserRepository: IAddUserRepository) {
+    constructor(private readonly addUserRepository: IAddUserRepository) {
 
-	}
-	async Execute(params: CreateUserParams): Promise<void> {
-		if (!params.name) {
-			throw new ValidationError("CreateUserParams", "name", "NAME_REQUIRED")
-		}
-		if (!params.email) {
-			throw new ValidationError("CreateUserParams", "email", "EMAIL_REQUIRED")
-		}
+    }
+    async Execute(params: CreateUserParams): Promise<void> {
+        if (!params.name) {
+            throw new ValidationError("CreateUserParams", "name", "NAME_REQUIRED")
+        }
+        if (!params.email) {
+            throw new ValidationError("CreateUserParams", "email", "EMAIL_REQUIRED")
+        }
 
-		if (!REGEX_EMAIL.test(params.email)) {
-			throw new ValidationError("CreateUserParams", "email", "INVALID_EMAIL")
-		}
+        if (!REGEX_EMAIL.test(params.email)) {
+            throw new ValidationError("CreateUserParams", "email", "INVALID_EMAIL")
+        }
 
-		if (!params.password) {
-			throw new ValidationError("CreateUserParams", "password", "PASSWORD_REQUIRED")
-		}
+        if (!params.password) {
+            throw new ValidationError("CreateUserParams", "password", "PASSWORD_REQUIRED")
+        }
+        if (!REGEX_PASSWORD.test(params.password)) {
+            throw new ValidationError("CreateUserParams", "password", "PASSWORD_NOT_STRONG_ENOUGH")
+        }
 
-		await this.addUserRepository.Add(new User(null, params.name, params.email, params.password))
-	}
+        const newUser = new User(null, params.name, params.email, params.password);
+        await this.addUserRepository.Add(newUser)
+    }
 }
