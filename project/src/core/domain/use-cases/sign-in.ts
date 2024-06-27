5248227303a7a14c3ddfece22de9623c1658c11a
Turbobot <turbobot@vercel.com>
2024-06-27 23:06:48 +0100

    feat(create-turbo): apply official-starter transform

--- project/src/core/domain/use-cases/sign-in.ts
+++ project/src/core/domain/use-cases/sign-in.ts
@@ -16,12 +16,9 @@ export class SignIn {
     constructor(private readonly getUserByEmailRepository: IGetUserByEmailRepository, private readonly hashCompare: IHashComparer) { }
 
     async Execute(params: SignInParams): Promise<SignInResult> {
-        console.log("Micael")
         const user = await this.getUserByEmailRepository.GetByEmail(params.username)
-        console.log(user)
         if (user) {
             const isPasswordValid = await this.hashCompare.Compare(user.passwordHash, params.password)
-            console.log(isPasswordValid)
             if (isPasswordValid) {
                 return {
                     token: "some_token"
