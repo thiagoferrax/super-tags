fc75b0cc9508b8cd6d068a56584b7790a253314d
Micael Costa <micael.costa@celfinet.com>
2024-07-04 00:51:11 +0100

    add correct validation on create user

--- project/apps/next-app/src/app/api/users/route.ts
+++ project/apps/next-app/src/app/api/users/route.ts
@@ -1,38 +1,66 @@
 import { ValidationError } from "@repo/domain";
 import { MakeCreateUser, MakeUserRepository } from "@repo/main";
 
-export const GET = async () => {
-	try {
-		const users = await MakeUserRepository().GetAll();
-		console.log("Micael 1")
 
-		return Response.json(users)
-	} catch (err: any) {
-		console.error(err)
-		return new Response("UNEXPECTED_ERROR", { status: 500 })
-	}
+class HttpResponseUtils {
+
+    private static GenerateReponse(body: object | undefined, statusCode: number): Response {
+        console.log(body)
+        return new Response(JSON.stringify(body), { status: statusCode })
+    }
+
+    private static GenerateError(errorCode: string, statusCode: number): Response {
+        return HttpResponseUtils.GenerateReponse({ errorCode }, statusCode)
+    }
+
+    static Ok(body: object): Response {
+        return HttpResponseUtils.GenerateReponse(body, 200)
+    }
+
+    static Created(): Response {
+        return HttpResponseUtils.GenerateReponse(undefined, 201)
+    }
+
+    static BadRequestError(errorCode: string): Response {
+        return HttpResponseUtils.GenerateError(errorCode, 400)
+    }
+
+    static UnexpectedError(): Response {
+        return HttpResponseUtils.GenerateError("UNEXPECTED_ERROR", 500)
+    }
+}
+
+export const GET = async () => {
+    try {
+        const users = await MakeUserRepository().GetAll();
+        return Response.json(users)
+    } catch (err: any) {
+        console.error(err)
+        return HttpResponseUtils.UnexpectedError()
+    }
 }
 
 export const POST = async (request: Request) => {
-	try {
-		const createUserUseCase = MakeCreateUser()
+    try {
+        const createUserUseCase = MakeCreateUser()
 
-		const body = await request.json()
-		createUserUseCase.Execute({
-			name: body.name, email: body.email, password: body.password
-		})
-		return new Response("Success", { status: 201 })
-	} catch (err: any) {
-		console.log(err)
-		if (err instanceof ValidationError) {
-			return new Response(err.code, { status: 400 })
-		}
-		return new Response("UNEXPECTED_ERROR", { status: 500 })
-	}
+        const body = await request.json()
+        await createUserUseCase.Execute({
+            name: body.name, email: body.email, password: body.password
+        })
+        return HttpResponseUtils.Created()
+    } catch (err: any) {
+        console.error(err)
+        if (err instanceof ValidationError) {
+            console.log("Micael")
+            return HttpResponseUtils.BadRequestError(err.code)
+        }
+        return HttpResponseUtils.UnexpectedError()
+    }
 
 }
 
 export const DELETE = async (request: Request) => {
-	await MakeUserRepository().ClearAll();
-	return new Response(undefined, { status: 204 })
+    await MakeUserRepository().ClearAll();
+    return new Response(undefined, { status: 204 })
 }
--- project/packages/domain/src/models/user.ts
+++ project/packages/domain/src/models/user.ts
@@ -2,27 +2,26 @@ import { ValidationError } from "../errors";
 import { Email } from "./email";
 
 export class User {
-	public readonly email: Email
-	public readonly id: number | null
-	public readonly name: string
-	public readonly passwordHash: string
-	constructor(
-		id: number | null,
-		name: string,
-		email: string,
-		passwordHash: string
-	) {
-		if (!name) {
-			throw new ValidationError("User", "name", "NAME_REQUIRED")
-		}
+    public readonly email: Email
+    public readonly id: number | null
+    public readonly name: string
+    public readonly passwordHash: string
+    constructor(
+        id: number | null,
+        name: string,
+        email: string,
+        passwordHash: string
+    ) {
+        if (!name) {
+            throw new ValidationError("User", "name", "NAME_REQUIRED")
+        }
 
-		if (!passwordHash) {
-			throw new ValidationError("User", "password", "PASSWORD_REQUIRED")
-		}
-		this.email = new Email(email)
-		console.log(passwordHash)
-		this.passwordHash = passwordHash
-		this.id = id
-		this.name = name
-	}
+        if (!passwordHash) {
+            throw new ValidationError("User", "password", "PASSWORD_REQUIRED")
+        }
+        this.email = new Email(email)
+        this.passwordHash = passwordHash
+        this.id = id
+        this.name = name
+    }
 }
